2c70902ec867edb2bf209cff4eae6fb6c4a416da
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-23 15:34:35 +0100

    object commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+
+// Accessing Object Properties
+objectName.propertyName  //objectName["propertyName"]
+person.lastName;      //person["lastName"];
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+// Accessing Object Methods
+objectName.methodName()
+name = person.fullName();
